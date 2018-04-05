5d33b28da6068f0da83ad89b80d003bbc5dfc06a
Abhinav Kasamsetty <abhinavkasamsetty@gmail.com>
2018-04-04 23:00:47 -0500

    Added code to handle server errors

--- notifier.py
+++ notifier.py
@@ -1,6 +1,7 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from datetime import datetime
 import praw
+import prawcore
 import telegram
 import config as cfg
 
@@ -14,44 +15,63 @@ sched = BlockingScheduler()
 
 cache = []
 
+
 def print_title(title, permalink):
+    """Prints post title to console and adds post link to cache
+    """
     cache.append(permalink)
+    print(title)
     return (title + ' https://www.reddit.com' + permalink + '\n\n')
 
 
 @sched.scheduled_job('interval', seconds=cfg.manga_interval)
 def scrape_manga():
+    """Searches Reddit for each manga in the list for new discussion posts
+    """
     print('Checking for manga at ' + str(datetime.now()))
     output = ''
     for manga in cfg.manga_list:
-        for submission in reddit.subreddit('manga').search(manga, sort='new', time_filter='day'):
-            title = submission.title.lower()
-            if '[disc]' in title and submission.permalink not in cache:
-                output += print_title(submission.title, submission.permalink)
+        try:
+            for submission in reddit.subreddit('manga').search(manga, sort='new', time_filter='day'):
+                title = submission.title.lower()
+                if '[disc]' in title and submission.permalink not in cache:
+                    output += print_title(submission.title, submission.permalink)
+        except prawcore.exceptions.ServerError as e:
+            print('Error when searching for ' + manga)
     if output != '':
         bot.send_message(chat_id=cfg.telegram_id, text='MANGA:\n\n' + output)
 
 
 @sched.scheduled_job('interval', seconds=cfg.re_zero_interval)
 def scrape_re_zero():
+    """Searches Reddit for new Re:Zero discussion posts
+    """
     print('Checking for new Re:Zero chapter at ' + str(datetime.now()))
     output = ''
-    for submission in reddit.subreddit('re_zero').search('[Translation]', sort='new', time_filter='day'):
-        if '[Translation]' in submission.title and not submission.is_self and submission.permalink not in cache:
-            output += print_title(submission.title, submission.permalink)
+    try:
+        for submission in reddit.subreddit('re_zero').search('[Translation]', sort='new', time_filter='day'):
+            if '[Translation]' in submission.title and not submission.is_self and submission.permalink not in cache:
+                output += print_title(submission.title, submission.permalink)
+    except prawcore.exceptions.ServerError as e:
+        print('Error when searching for Re:Zero')
     if output != '':
         bot.send_message(chat_id=cfg.telegram_id, text='RE:ZERO:\n\n' + output)
 
 
 @sched.scheduled_job('interval', seconds=cfg.anime_interval)
 def scrape_anime():
+    """Searches Reddit for each anime in the list for new discussion posts
+    """
     print('Checking for anime at ' + str(datetime.now()))
     output = ''
     for anime in cfg.anime_list:
-        for submission in reddit.subreddit('anime').search(anime, sort='new', time_filter='day'):
-            title = submission.title.lower()
-            if '[spoilers]' in title and 'discussion' in title and submission.is_self and submission.permalink not in cache:
-                output += print_title(submission.title, submission.permalink)
+        try:
+            for submission in reddit.subreddit('anime').search(anime, sort='new', time_filter='day'):
+                title = submission.title.lower()
+                if '[spoilers]' in title and 'discussion' in title and submission.is_self and submission.permalink not in cache:
+                    output += print_title(submission.title, submission.permalink)
+        except prawcore.exceptions.ServerError as e:
+            print('Error when searching for ' + anime)
     if output != '':
         bot.send_message(chat_id=cfg.telegram_id, text='ANIME:\n\n' + output)
 
